22f8e089dcd0e63e7d746d0e8fe71aad207f2725
xinrantaoangela <xinran.tao2001@gmail.com>
2025-12-09 08:33:09 -0500

    Initial Commit

---
+++ Visual Invisible/Visual_InvisibleApp.swift
@@ -0,0 +1,17 @@
+//
+//  Visual_InvisibleApp.swift
+//  Visual Invisible
+//
+//  Created by Angela Tao on 12/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Visual_InvisibleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
